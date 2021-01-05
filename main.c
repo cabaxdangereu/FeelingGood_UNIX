5ff58ddd46dd5fa4aac011c487211b49b23824fe
Dimitri <74330293+cabaxdangereu@users.noreply.github.com>
2021-01-05 23:11:34 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main( int argc, char *argv [] )
+{
+    int number;
+    while(number<=10)
+    {
+      printf("%d\n",number);
+      number++;
+    } 
+}
