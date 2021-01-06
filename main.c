db9e8dea6a5ab8dff2d3d29cce514af157427d7f
Dimitri VERGOZ <dimitri.vergoz@edu.devinci.fr>
2021-01-06 21:35:44 +0100

    La premiere V ne marchait pas... Maintenant c'est bon.

--- main.c
+++ main.c
@@ -1,14 +1,8 @@
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-int main( int argc, char *argv [] )
-{
-    int number;
-    while(number<=10)
-    {
-      printf("%d\n",number);
-      number++;
-    } 
+#include<stdio.h>
+int main(){
+  int n =1;
+  while(n<=10){
+    printf("%d\n",n);
+    n++;
+}
 }
